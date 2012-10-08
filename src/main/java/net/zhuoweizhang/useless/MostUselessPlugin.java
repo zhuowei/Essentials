4863a26178fc19e70337abbca2ec34bd681af32b
Ollie <dzineit@live.co.uk>
2012-10-08 19:11:38 +0200

    Less code is win!
    Signed-off-by: Ollie <dzineit@live.co.uk>

--- src/main/java/net/zhuoweizhang/useless/MostUselessPlugin.java
+++ src/main/java/net/zhuoweizhang/useless/MostUselessPlugin.java
@@ -1,18 +1,15 @@
 package net.zhuoweizhang.useless;
 import org.spout.api.plugin.CommonPlugin;
-import org.spout.api.plugin.PluginDescriptionFile;
 public class MostUselessPlugin extends CommonPlugin {
-	private PluginDescriptionFile pdfFile;
-
 	@Override
 	public void onDisable() {
-		getLogger().info(pdfFile.getFullName() + " has been disabled!");
+		getLogger().info(getName() + " has been disabled!");
 	}
 
 	@Override
 	public void onEnable() {
 		pdfFile = getDescription();
-		getLogger().info(pdfFile.getFullName() + " has been enabled!");
+		getLogger().info(getName() + " has been enabled!");
 		getEngine().getPluginManager().disablePlugin(this);
 	}
 }
